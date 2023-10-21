1ae25ba8bbb27544282a11dea1332202690a668d
Douglas <cbdouglasvirissimo@hotmail.com>
2023-10-21 14:29:57 -0300

    feat: add repository - AddressRepository and PersonRepository

---
+++ src/main/java/one/digitalinnovation/patterns/model/PersonRepository.java
@@ -0,0 +1,8 @@
+package one.digitalinnovation.patterns.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends CrudRepository<Person, Long> {
+}
